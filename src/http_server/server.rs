15cd6d5da183edc592aa64525589bd06f83fe2f8
Fahad Rizvi <sfahadrizvi@gmail.com>
2024-02-28 21:08:40 -0600

    Adding endpoints and storing keys values as files

--- src/http_server/server.rs
+++ src/http_server/server.rs
@@ -1,36 +1,86 @@
-use axum::extract::Path;
+use std::{collections::HashMap, fs::{self, OpenOptions}, io::Write};
+
+use axum::{extract::{Path, Query, State}, http::StatusCode};
 
 //PUT Request
-pub(crate) async fn create_key(Path(key): Path<String>, value: String) {
-    println!("Create key {} value {}", key, value);
-    ()
+pub(crate) async fn create_key(Query(params): Query<HashMap<String, String>>, State(state): State<String>, body:String)
+ -> Result<(), StatusCode> {
+    create_or_update_key(Query(params), State(state), true)
 }
 
 //Patch Request
-pub(crate) async fn update_key(Path(key): Path<String>, value: String) {
-    println!("Update key {} value {}", key, value);
-    ()
+pub(crate) async fn update_key(Query(params): Query<HashMap<String, String>>, State(state): State<String>, value: String)
+-> Result<(), StatusCode> {
+    create_or_update_key(Query(params), State(state), false)
 }
 
 //Delete Request
-pub(crate) async fn delete_key(Path(key): Path<String>, value: String) {
+pub(crate) async fn delete_key(Path(key): Path<String>, State(state): State<String>, value: String)
+-> Result<(), StatusCode> {
     println!("delete key {} value {}", key, value);
-    ()
+    let path = format!("{}/{}", state, key);
+    if !std::path::Path::new(&path).exists() {
+        println!("deleteting nonexisting key {}", key);
+        Ok(())
+    } else {
+        match fs::remove_file(path) {
+            Ok(()) => Ok(()),
+            Err(err) => {
+                println!("Key could not be delted: {}", err);
+                Err(StatusCode::NOT_MODIFIED)
+            }
+        }
+    }
 }
 
 ////Get Request
-pub(crate) async fn get_key_value(Path(key): Path<String>, value: String) {
+pub(crate) async fn get_value(Path(key): Path<String>, State(state): State<String>, value: String)
+-> Result<(), StatusCode> {
     println!("Get key {} value {}", key, value);
-    ()
+    Ok(())
 }
 
-////Get Request
-pub(crate) async fn get_prefix_key_value(Path(key): Path<String>, value: String) {
-    println!("Get key with prefix {} value {}", key, value);
-    ()
-}
 ////Get Request with query parameter ?all=true
-pub(crate) async fn get_keys() {
+pub(crate) async fn get_keys() -> Result<(), StatusCode> {
     println!("Get all key");
-    ()
+    Ok(())
+}
+
+fn create_or_update_key(
+    Query(params): Query<HashMap<String, String>>,
+    State(state): State<String>,
+    create_new: bool
+)
+ -> Result<(), StatusCode> {
+    println!("{} key {} value {} with state {}", if create_new {"Create" } else {"Update"}, params["key"], params["value"], state);
+    
+    if params.get("key").is_none() || params.get("value").is_none() {
+        return Err(StatusCode::BAD_REQUEST);
+    }
+    
+    let path = format!("{}/{}", state, params["key"]);
+    if path.split("/").count() != 2 {
+        return Err(StatusCode::BAD_REQUEST);
+    }
+
+    let file = OpenOptions::new()
+                            .create_new(create_new)
+                            .create(true)
+                            .write(true)
+                            .truncate(true)
+                            .open(path);
+                            
+    match file {
+        Ok(mut file) => {
+            let ret = file.write_all(params["value"].as_bytes());
+            match ret { 
+                Ok(()) => Ok(()),
+                Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR)
+            }
+        },
+        Err(err) => {
+            println!("Error: {}", err);
+            Err(StatusCode::NOT_MODIFIED)
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,58 @@
 pub mod http_server;
 
 use axum::{
-    routing::{delete, get, patch, put}, Router,
+    routing::{get, post}, Router
 };
-
+use clap::Parser;
 use http_server::server::{
     create_key, 
     update_key, 
     delete_key, 
-    get_key_value, 
-    get_prefix_key_value, 
-    get_keys
+    get_value
 };
+use std::path::Path;
 
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// The port to listen on
+    #[arg(short, long)]
+    port: i32,
+
+    /// The path to the key-value store
+    #[arg(long, default_value = "./ssd")]
+    path: String,
+}
 
 #[tokio::main()]
-async fn main() {
+async fn main() -> Result<(), String> { 
+    
+    let args = Args::parse();
+
+    if !Path::new(&args.path).exists() {
+        println!("The path to key value store does not exist");
+        return Err("The path to key value store does not exist".to_string())
+    }
 
     // build our application with a route
     let app = Router::new()
-        .route("/key/:key", put(create_key))
-        .route("/key/:key", patch(update_key))
-        .route("/key/:key", delete(delete_key))
-        .route("/key/:key", get(get_key_value))
-        .route("/key_prefix/:key", get(get_prefix_key_value))
-        .route("/keys", get(get_keys));
+        .route(INSERT_END_POINT, post(create_key))
+        .route(INSERT_UPDATE_END_POINT, post(update_key))
+        .route(DELETE_END_POINT, post(delete_key))
+        .route(GET_END_POINT, get(get_value))
+        .with_state(args.path.clone());
 
-    // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let uri = format!("0.0.0.0:{}", args.port);
+    let listener = tokio::net::TcpListener::bind(uri).await.unwrap();
     axum::serve(listener, app).await.unwrap();
+
+    Ok(())
 }
+
+static GET_END_POINT: &str = "/get";
+static INSERT_END_POINT: &str = "/insert";
+static INSERT_UPDATE_END_POINT: &str = "/update";
+static DELETE_END_POINT: &str = "/delete";
 
 
